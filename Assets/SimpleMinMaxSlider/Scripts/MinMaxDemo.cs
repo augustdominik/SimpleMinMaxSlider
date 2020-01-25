21f7762eb42e91ffdea6e271436b58a62c0e6b57
GucioDevs <guciodevs@gmail.com>
2020-01-26 00:42:21 +0100

    Changed attribute name from MinMax to MinMaxSlider.

--- Assets/SimpleMinMaxSlider/Scripts/MinMaxDemo.cs
+++ Assets/SimpleMinMaxSlider/Scripts/MinMaxDemo.cs
@@ -5,10 +5,10 @@ public class MinMaxDemo : MonoBehaviour {
 
     public string info = "Hover over slider to see tooltip";
 
-    [MinMax(0,10)] 
+    [MinMaxSlider(0,10)] 
     public Vector2 floatTest = new Vector2(2f, 8f);
 
-    [MinMax(0,10)]
+    [MinMaxSlider(0,10)]
     public Vector2Int intTest;
     
 }
