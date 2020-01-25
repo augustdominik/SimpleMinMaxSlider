acb034090bd9b355194daa814d500711c0175094
GucioDevs <guciodevs@gmail.com>
2020-01-26 00:31:32 +0100

    SimpleMinMaxSlider finished.

---
+++ Assets/SimpleMinMaxSlider/Scripts/MinMaxAttribute.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MinMaxAttribute : PropertyAttribute{
+
+    public float min;
+    public float max;
+
+    public MinMaxAttribute(float min, float max)
+    {
+        this.min = min;
+        this.max = max;
+    }
+}
---
+++ Assets/SimpleMinMaxSlider/Scripts/MinMaxDemo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class MinMaxDemo : MonoBehaviour {
+
+    public string info = "Hover over slider to see tooltip";
+
+    [MinMax(0,10)] 
+    public Vector2 floatTest = new Vector2(2f, 8f);
+
+    [MinMax(0,10)]
+    public Vector2Int intTest;
+    
+}
