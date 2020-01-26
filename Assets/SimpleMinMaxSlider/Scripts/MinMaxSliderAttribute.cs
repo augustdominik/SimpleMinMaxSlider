9176bd7e8bed8a26115a956ec8f97f6de9ecf511
GucioDevs <guciodevs@gmail.com>
2020-01-26 12:54:00 +0100

    Added namespace: MinMaxSlider.

--- Assets/SimpleMinMaxSlider/Scripts/MinMaxSliderAttribute.cs
+++ Assets/SimpleMinMaxSlider/Scripts/MinMaxSliderAttribute.cs
@@ -1,13 +1,16 @@
-﻿using UnityEngine;
+﻿namespace MinMaxSlider
+{
+    using UnityEngine;
 
-public class MinMaxSliderAttribute : PropertyAttribute{
+    public class MinMaxSliderAttribute : PropertyAttribute{
 
-    public float min;
-    public float max;
+        public float min;
+        public float max;
 
-    public MinMaxSliderAttribute(float min, float max)
-    {
-        this.min = min;
-        this.max = max;
-    }
+        public MinMaxSliderAttribute(float min, float max)
+        {
+            this.min = min;
+            this.max = max;
+        }
+    }  
 }
