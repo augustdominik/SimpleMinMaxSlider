27a03986c12d3b35608942a3a5a908add6421094
Adam Brzozowski <adamcbrz@gmail.com>
2020-09-25 09:52:06 -0500

    Restructered plugin and addressed issue with building
    Reference: https://github.com/GucioDevs/SimpleMinMaxSlider/issues/2

---
+++ Assets/SliderTest.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GD.MinMaxSlider;
+public class SliderTest : MonoBehaviour
+{
+    [MinMaxSlider(0, 100)]
+    public Vector2 slider;
+}
