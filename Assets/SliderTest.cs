51389e3ab1e33933ba8328acd7871e3887f88c9e
Adam Brzozowski <adamcbrz@gmail.com>
2020-09-25 10:21:13 -0500

    Removed test files

--- Assets/SliderTest.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using GD.MinMaxSlider;
-public class SliderTest : MonoBehaviour
-{
-    [MinMaxSlider(0, 100)]
-    public Vector2 slider;
-}
